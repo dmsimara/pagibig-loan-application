ac0c2fa631e9bc89c4135a3ce4bb41e6e23f64fe
Daniella Simara <150517240+dmsimara@users.noreply.github.com>
2025-03-16 19:54:32 +0800

    initial commit

---
+++ PagibigApplication/src/main/java/com/mycompany/pagibigapplication/PagibigApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pagibigapplication;
+
+/**
+ *
+ * @author Daniella Simara
+ */
+public class PagibigApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
